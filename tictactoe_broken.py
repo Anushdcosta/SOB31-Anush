017b56b58290f023a880688ce9b54cdb3ebb287d
Anushdcosta <Anushviston@gmail.com>
2023-11-28 17:01:19 +0400

    tictactoe_broken.py file fixed

--- tictactoe_broken.py
+++ tictactoe_broken.py
@@ -67,7 +67,7 @@ def add_piece(game, player, row, column):
     row: 0-index row
     column: 0-index column
     """
-    game[row][column+1] = player
+    game[row][column] = player #removing the +1 in the column in order to ensure the index isn't larger that the length of the list.
     return game
 
 def check_space_empty(game, row, column):
@@ -77,7 +77,7 @@ def convert_input_to_coordinate(user_input):
     return user_input - 1
 
 def switch_player(player):
-    if player = 1:
+    if player == 1: #assignment operator to Comparison operator (= to ==)
         return 2
     else:
         return 1
@@ -98,12 +98,14 @@ if __name__ == '__main__':
     while winner == 0 and moves_exist(game):
         print("Currently player: " + str(player))
         available = False
-        while not available
+        while not available: #colon added
             row = convert_input_to_coordinate(int(input("Which row? (start with 1) ")))
             column = convert_input_to_coordinate(int(input("Which column? (start with 1) ")))
-            available = check_space_empty(game, row)
+            available = check_space_empty(game, row, column)#third argument column added 
+            if available == False:
+                print("That box is already marked.. Please select another one.")
         game = add_piece(game, player, row, column)
         display_game(game)
         player = switch_player(player)
-#        winner = check_winner(game)
+        winner = check_winner(game)#comment removed
     display_winner(winner)
